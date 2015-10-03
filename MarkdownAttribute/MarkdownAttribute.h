1ed53d99444549cb25471740b3adaae9378a63a1
Junyuan Hong <jyhong836@gmail.com>
2015-10-03 21:34:10 +0800

    Initialise xcode project.

---
+++ MarkdownAttribute/MarkdownAttribute.h
@@ -0,0 +1,19 @@
+//
+//  MarkdownAttribute.h
+//  MarkdownAttribute
+//
+//  Created by Junyuan Hong on 10/3/15.
+//  Copyright © 2015 Junyuan Hong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MarkdownAttribute.
+FOUNDATION_EXPORT double MarkdownAttributeVersionNumber;
+
+//! Project version string for MarkdownAttribute.
+FOUNDATION_EXPORT const unsigned char MarkdownAttributeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarkdownAttribute/PublicHeader.h>
+
+

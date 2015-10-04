d156ca929597e3dca15325a1c11d9f67e5954b1a
Junyuan Hong <jyhong836@gmail.com>
2015-10-04 17:19:37 +0800

    Copy markdown test sets and license.

--- MarkdownAttributeTests/MarkdownAttributeTests.swift
+++ MarkdownAttributeTests/MarkdownAttributeTests.swift
@@ -3,8 +3,28 @@
 //  MarkdownAttributeTests
 //
 //  Created by Junyuan Hong on 10/3/15.
-//  Copyright © 2015 Junyuan Hong. All rights reserved.
 //
+//    The MIT License (MIT)
+//
+//    Copyright (c) 2015 Junyuan Hong
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
 
 import XCTest
 @testable import MarkdownAttribute
@@ -22,13 +42,45 @@ class MarkdownAttributeTests: XCTestCase {
     }
     
     func testExample() {
+        let mstring = [
+            "# Playground",
+            "+ hello, **world**",
+            "+ byebe, **world**",
+            "",
+            "1. this is numbered list item 1",
+            "2. this is numbered list item 1, and a **em**, a *strong*",
+            "",
+            "link: [website](jyhong.com)",
+            "",
+            "1. ok",
+            "   + hi",
+            "   + hello",
+            "1. fine",
+            "",
+            "```swift",
+            "func swiftFunc()",
+            "   print(\"hi\")",
+            "```",
+            "😄汉字, `inline code`, r",
+            "",
+            "<a href=\"jyhong.com\">jyho</a>",
+            "end"
+            ].joinWithSeparator("\n")
+        
+        do {
+            let astr = try MAMarkdown.attributedString(markdown: mstring, extensions: MMMarkdownExtensions.GitHubFlavored)
+        } catch {
+            NSLog("\(error)")
+        }
+        
+
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measureBlock {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
     
 }
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # MarkdownAttribute
 A Markdown parser for converting Markdown string to NSAttributedString.
 
-This project uses [MMMarkdown](https://github.com/mdiep/MMMarkdown) to parse markdown string, and use some code from [CocoaMarkdown](https://github.com/indragiek/CocoaMarkdown) to convert them to NSAttributedString. The license of them have been included in 'LICENSE' file.
+This project uses [MMMarkdown](https://github.com/mdiep/MMMarkdown) to parse markdown string, and use some code from [CocoaMarkdown](https://github.com/indragiek/CocoaMarkdown) to convert them to NSAttributedString. For testing, the test codes from [Markingbird](https://github.com/kristopherjohnson/Markingbird) is used, which are translated from [MarkingSharp](https://code.google.com/p/markdownsharp/). The test benchmarks are original from website, which have been illustrated in `!readme.txt` file in test folders. The license of them have been included in 'LICENSE' file.
 
 ### TODO
 
